7be46f79066a37a05e261b0cce9654f6dea84568
parakh.pixel6 <parakh@pixel6.co>
2023-09-27 18:11:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { SidenavComponent } from './sidenav/sidenav.component';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    // SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu.ts
@@ -0,0 +1,17 @@
+export interface Menu {
+    menu: string,
+    icon: string,
+}
+
+
+export const menus = [
+    {
+        menu: 'Home',
+        icon: 'home'
+    },
+
+    {
+        menu: 'Products',
+        icon: 'diamond'
+    }
+]
